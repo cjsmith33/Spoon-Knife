9df77ad47ed2a4941aff8f5afb3f06806b155979
cjsmith33 <charleswsmith8@gmail.com>
2021-05-19 18:31:00 -0700

    added a javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+const country = 'Unitied State';
+const continent = 'North America';
+let population = '400 million';
+let isIsland = true
+let language = 'English'
+
+console.log(country)
+console.log(continent)
+console.log(population)
+console.log(isIsland)
+console.log(language)
+//continent = 'BBB'
+console.log(typeof country)
